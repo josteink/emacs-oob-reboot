e15edbeb8b74654107394717afbeee8dbc34c851
Jostein Kjønigsen <jostein@kjonigsen.net>
2017-05-05 11:22:51 +0200

    Proposed changes to nxml-mode.

---
+++ init.el
@@ -0,0 +1,5 @@
+
+;; this init.el file should only be used to load changes organized in sibling
+;; elisp-files.
+
+(load-file "lisp/nxml-mode-changes.el")
---
+++ lisp/nxml-mode-changes.el
@@ -0,0 +1,3 @@
+
+;; causes entire elements (with children) to be treated as sexps.
+(setq nxml-sexp-element-flag t)
