e0ed22fc3df8db61746a74b7e8134a48035a9b91
Mathieu Marques <mathieumarques78@gmail.com>
2017-05-23 12:01:19 +0200

    Prefer regexp isearch

--- lisp/isearch-changes.el
+++ lisp/isearch-changes.el
@@ -1,3 +1,6 @@
+(define-key global-map [remap isearch-forward] 'isearch-forward-regexp)
+(define-key global-map [remap isearch-backward] 'isearch-backward-regexp)
+
 (setq-default
  isearch-allow-scroll t                 ; Allow scrolling in an isearch session
  lazy-highlight-cleanup nil             ; Leave highlights after an isearch session
