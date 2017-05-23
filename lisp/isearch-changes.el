23fa8130f21ff35cb5cc2c0b8daf6afdc2b70f5d
Mathieu Marques <mathieumarques78@gmail.com>
2017-05-23 11:27:37 +0200

    Add isearch changes

---
+++ lisp/isearch-changes.el
@@ -0,0 +1,4 @@
+(setq-default
+ isearch-allow-scroll t                 ; Allow scrolling in an isearch session
+ lazy-highlight-cleanup nil             ; Leave highlights after an isearch session
+ lazy-highlight-initial-delay 0)        ; Start highlighting immediately
